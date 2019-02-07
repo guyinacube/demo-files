01beb999af757279febc0002c4948cfe5318e94f
M4Y <ffamaster.z@gmail.com>
2019-02-07 14:52:20 +0100

    Fixed several issues
    - Define endpoint once, outside loop
    - Fix memory percentage calculation
    - Fixed `$Time` variable containing months instead of minutes
    - Log payload to console
    - Introduced small delay

--- video demos/PowerShell/20190206 - Streaming dataset.ps1
+++ video demos/PowerShell/20190206 - Streaming dataset.ps1
@@ -1,22 +1,24 @@
-﻿while($true)
+$endpoint = "[your endpoint here]"
+
+while($true)
 {
 
     $ComputerCPU = (Get-WmiObject  -Class win32_processor -ErrorAction Stop | Measure-Object -Property LoadPercentage -Average | Select-Object Average).Average
 
     $ComputerMemory = Get-WmiObject  -Class win32_operatingsystem -ErrorAction Stop
-    $Memory = ((($ComputerMemory.TotalVisibleMemorySize - $ComputerMemory.FreePhysicalMemory)*100)/ $ComputerMemory.TotalVisibleMemorySize)
+    $UsedMemory = $ComputerMemory.TotalVisibleMemorySize - $ComputerMemory.FreePhysicalMemory
+    $Memory = (($UsedMemory/ $ComputerMemory.TotalVisibleMemorySize)*100)
     $RoundMemory = [math]::Round($Memory, 2)
 
     $Date = Get-Date -DisplayHint Date -Format MM/dd/yyyy
 
-    $Time = Get-Date -DisplayHint Time -Format HH:MM:ss
+    $Time = Get-Date -DisplayHint Time -Format HH:mm:ss
 
     #$RoundMemory
     #$ComputerCPU 
     #$Date
     #$Time
 
-    $endpoint = "https://api.powerbi.com/beta/a7502f9c-15b1-4eae-a9bc-d3311a46aaf7/datasets/27cb708d-edc1-4856-bd53-6371218099b6/rows?key=HsJocycMmC%2BTHOoLcq6bvcPLhrcbEfSQFaKj5X3eF0%2Fyu1sRSInaP%2BRaqoqrIXuJwpQuQh0hjcurg%2BMLG8WljQ%3D%3D"
     $payload = @{
     "Date" =$Date
     "Time" =$Time
@@ -25,6 +27,7 @@
     }
     Invoke-RestMethod -Method Post -Uri "$endpoint" -Body (ConvertTo-Json @($payload))
 
-
-
+    Write-Host "Date: " $Date " Time: " $Time " CPU: " $ComputerCPU " Memory: " $RoundMemory
+    
+    sleep 2
 }
